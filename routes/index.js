f13fccc40ff4b72fc629f1b30819caa4ce4961b2
ivanivanovicdimov <55855675+ivanivanovicdimov@users.noreply.github.com>
2020-06-22 21:44:30 -0700

    Updated index.js

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,13 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
+
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  //res.send('respond with a resource');
+  var title = "Our Home Page";
+  res.render('pages/index',{
+    title:title
+  });
 });
 
 module.exports = router;
